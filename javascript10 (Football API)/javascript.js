6e492f155102bc841966915f74aa84e5e625ca12
unknown <paul.hartmann@student.htldornbirn.at>
2021-12-08 23:04:47 +0100

    neu

--- javascript10 (Football API)/javascript.js
+++ javascript10 (Football API)/javascript.js
@@ -1,7 +1,24 @@
-function abfrage(liga) {
+function abfrage() {
+
+  liga = document.querySelector("#liga").value;;
 
 
-url = 'https://api.football-data.org/v2/competitions/'+liga+'/scorers'
+
+url1 = 'https://api.football-data.org/v2/competitions/'+liga+'/scorers'
+
+
+
+//url1 = 'https://api.football-data.org/v2/competitions/'
+
+
+
+  datenholen(url1);
+}
+
+
+function datenholen(url) {
+
+
 
 fetch(url,
 
@@ -10,7 +27,23 @@ headers: {
   "x-auth-token": "1b09c3488b9c4bd5827930e1a049e5e7"
 }})
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => spielerausgeben(data));
+
+
+
 
 }
 
+
+function spielerausgeben(daten) {
+
+
+  ausgabe="";
+  console.log(daten);
+
+
+  for (i = 0; i < daten.scorers.length; i++) {
+    ausgabe+= daten.scorers[i].player.name+"         Tore:"+daten.scorers[i].numberOfGoals+"<br>";
+  }
+  document.getElementById('ausgabe').innerHTML = ausgabe;
+}
