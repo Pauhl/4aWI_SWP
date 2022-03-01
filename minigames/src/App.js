f0b326e862ed300aeda6eebcf6a7df7d7153b1a4
unknown <paul.hartmann@student.htldornbirn.at>
2022-03-01 10:18:53 +0100

    1

---
+++ minigames/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar.js';
+import Grid from './components/Grid.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Grid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ minigames/src/components/Grid.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+import styles from "./Grid.module.css";
+
+class Grid extends Component {
+
+
+    
+    
+    render() {
+        return (
+            
+            <div>
+                <p>games</p>
+            </div>
+           
+        )
+    }
+
+}
+
+export default Grid;
